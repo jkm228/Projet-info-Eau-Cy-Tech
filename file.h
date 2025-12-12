65800ead4916a770613360791581525c5ff43bbb
jkm228 <koutomessanjo@gmail.com>
2025-12-12 11:29:07 +0100

    Update file.h

--- file.h
+++ file.h
@@ -5,20 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-// On définit une constante pour la taille max d'une ligne
 #define TAILLE_LIGNE 1024
 
-// Prototypes des fonctions implémentées dans file.c
-
-// Ouvre un fichier et gère les erreurs (arrête le programme si échec)
 FILE* ouvrir_fichier(char* chemin, char* mode);
-
-// Ferme le fichier
 void fermer_fichier(FILE* f);
 
-// Lit une ligne du CSV et extrait les informations utiles
-// Retourne 1 si une ligne a été lue, 0 si fin de fichier
-// Utilise des pointeurs pour modifier les variables du main (Passage par adresse)
-int lire_ligne_csv(FILE* f, int* id_station, long* capacite, long* debit);
+// Modification : id_station devient un buffer de caractères (char*)
+int lire_ligne_csv(FILE* f, char* id_station, long* capacite);
 
 #endif
