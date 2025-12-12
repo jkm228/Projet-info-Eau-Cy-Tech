aa5742b8cab6e613637f6027781be0acd082549b
jkm228 <koutomessanjo@gmail.com>
2025-12-12 14:24:52 +0100

    Update file.h

--- file.h
+++ file.h
@@ -1,16 +1,9 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>   
+#include "avl.h" 
 
-#define TAILLE_LIGNE 1024
-
-FILE* ouvrir_fichier(char* chemin, char* mode);
-void fermer_fichier(FILE* f);
-
-// Modification : id_station devient un buffer de caractères (char*)
-int lire_ligne_csv(FILE* f, char* id_station, long* capacite);
+void chargerDonnees(const char* chemin_fichier, Station** racine, const char* option);
 
 #endif
