3b352e7ee50a2a5dfde5ba4ce05228ba23e90895
jkm228 <koutomessanjo@gmail.com>
2025-12-14 13:43:23 +0100

    Refactor header file and add function prototype
    Updated header guards and included function prototype.

--- main.h
+++ main.h
@@ -1,18 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_H
+#define FILE_H
 
-// Bibliothèques standards utilisées dans le main
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Inclusion de notre structure de données
 #include "wildwater.h"
 
-// Constantes
-#define TAILLE_BUFFER 1024
-
-// Si tu décides un jour de créer des fonctions spécifiques pour le parsing
-// (ex: traiter_ligne, verifier_arguments), leurs prototypes iront ici.
+// Prototype de la fonction implémentée dans file.c
+void chargerDonnees(const char* chemin_fichier, Station** racine, const char* option);
 
 #endif
