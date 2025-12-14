ed14cfed9e5d8abca9a148e7a0b53198f72d94e4
Adamazk <adamarezkii@gmail.com>
2025-12-14 19:07:44 +0100

    Replace wildwater.h with avl.h in main.h

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define FILE_H
 
 #include <stdio.h>
-#include "wildwater.h"
+#include "avl.h"
 
 // Prototype de la fonction implémentée dans file.c
 void chargerDonnees(const char* chemin_fichier, Station** racine, const char* option);
