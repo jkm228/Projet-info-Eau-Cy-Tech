9e6cac26e96176699d87fb42ff17d3d578ddfd3c
jkm228 <koutomessanjo@gmail.com>
2025-12-14 13:33:18 +0100

    Refactor data loading function in file.c

--- file.c
+++ file.c
@@ -1,65 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include "wildwater.h"
 #include "file.h"
 
+#define TAILLE_BUFFER 1024
+
 void chargerDonnees(const char* chemin_fichier, Station** racine, const char* option) {
     FILE* fichier = fopen(chemin_fichier, "r");
-
     if (fichier == NULL) {
-        fprintf(stderr, "Erreur d'ouverture du fichier %s : %s\n", chemin_fichier, strerror(errno));
+        perror("Erreur ouverture fichier");
         exit(1);
     }
 
-    char buffer[1024];
+    char buffer[TAILLE_BUFFER];
+    // On ignore la ligne d'en-tête si elle existe
+    // fgets(buffer, TAILLE_BUFFER, fichier); 
 
-    fgets(buffer, 1024, fichier);
-
-    while (fgets(buffer, 1024, fichier) != NULL) {
-        
-        int id_a = 0;
-        int id_b = 0;
+    while (fgets(buffer, TAILLE_BUFFER, fichier) != NULL) {
+        char id_station[50] = "";
         long capacite = 0;
-        long debit = 0;
+        char* token;
 
-        char* token = strtok(buffer, ";");
-        if (token != NULL && strcmp(token, "-") != 0) {
-            id_a = atoi(token);
+        // Le format de ton fichier est : -;ID_STATION;DEST;CAPACITE;...
+
+        // 1. On ignore la première colonne (-)
+        token = strtok(buffer, ";");
+
+        // 2. On lit l'ID de la station (C'est du texte !)
+        token = strtok(NULL, ";");
+        if (token != NULL) {
+            strncpy(id_station, token, 49);
+            id_station[49] = '\0'; // Sécurité
         }
 
+        // 3. On ignore la destination
+        token = strtok(NULL, ";");
+
+        // 4. On lit la capacité (C'est un chiffre)
         token = strtok(NULL, ";");
         if (token != NULL && strcmp(token, "-") != 0) {
-            id_b = atoi(token);
+            capacite = atol(token);
         }
 
-        token = strtok(NULL, ";");
-        if (token != NULL && strcmp(token, "-") != 0) {
-            if (strchr(token, '-') == NULL) {
-                 capacite = atol(token);
-            }
-        }
-
-        token = strtok(NULL, ";");
-        if (token != NULL && strcmp(token, "-") != 0) {
-             if (strchr(token, '-') == NULL) {
-                debit = atol(token);
-             }
-        }
-
-        if (strcmp(option, "max") == 0) {
-            if (id_a != 0 && capacite > 0) {
-                *racine = insererStation(*racine, id_a, "Station", 0, capacite);
-            }
-        }
-        else {
-            if (id_a != 0 && id_b != 0 && debit > 0) {
-                 *racine = insererStation(*racine, id_b, "Client", 0, debit);
-            }
-            else if (id_a != 0 && debit > 0) {
-                 *racine = insererStation(*racine, id_a, "Producteur", 0, debit);
-            }
+        // Si on a bien un ID et une capacité, on insère
+        if (strlen(id_station) > 0 && capacite > 0) {
+            // On passe 0 en id numérique car on utilise le texte
+            *racine = insererStation(*racine, 0, id_station, 0, capacite);
         }
     }
 
