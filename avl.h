4ac9742e3d40e54bf0063fb42491c8d045475d0c
jkm228 <koutomessanjo@gmail.com>
2025-12-16 22:56:20 +0100

    Update avl.h

--- avl.h
+++ avl.h
@@ -3,36 +3,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// On a retiré <string.h> pour faire "maison"
 
-// Définition de la structure conforme aux cours (fg/fd)
+// Structure Station modifiée pour gérer les réels
 typedef struct Station {
-    int id;                 // Identifiant numérique
-    char id_str[50];        // Identifiant texte
-    long capacite;          // Capacité (pour le max)
-    long conso;             // Consommation (pour src/real)
-    int h;                  // Hauteur du noeud
+    // int id; // Supprimé car les IDs sont textuels (ex: "Plant #JA...")
+    char id_str[50];        // Identifiant (Clé de l'arbre)
+    double capacite;        // Capacité (double pour gérer les virgules)
+    double conso;           // Consommation
+    int h;                  // Hauteur
     struct Station *fg;     // Fils gauche
     struct Station *fd;     // Fils droit
 } Station;
 
-typedef Station* pStation; // Pointeur vers Station (style L2)
+typedef Station* pStation;
 
 // Fonctions de base
 int max(int a, int b);
 int hauteur(pStation a);
-int equilibre(pStation a); // Facteur d'équilibre
+int equilibre(pStation a);
 
-// Rotations
+// Fonctions de rotation
 pStation rotationDroite(pStation y);
 pStation rotationGauche(pStation x);
 pStation doubleRotationGD(pStation a);
 pStation doubleRotationDG(pStation a);
 
 // Fonctions principales
-pStation creerStation(int id, char* code, long cap);
-pStation inserer(pStation a, int id, char* code, long cap, long flux);
-void infixe(pStation a, FILE* fs); // Parcours infixe inverse
+// Note : on passe des double pour cap et flux
+pStation creerStation(char* code, double cap);
+pStation inserer(pStation a, char* code, double cap, double flux);
+void infixe(pStation a, FILE* fs); 
 void liberer(pStation a);
 
 #endif
